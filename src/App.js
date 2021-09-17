a0b5d86d31fd2ea63c73bb46831236d57563ba72
thanhtb-2411 <tran.ba.thanh@sun-asterisk.com>
2021-09-17 23:10:22 +0700

    test deploy 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
+import logo from './ion.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <h2>
+              GitHub Actions & Docker
+          </h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Docker + Github action
+          Cảm ơn các bạn
         </p>
       
       </header>
