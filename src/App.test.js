ed2caa4631adb3974714173310e076c61f1eba4d
thanhtb-2411 <tran.ba.thanh@sun-asterisk.com>
2021-09-17 21:06:14 +0700

    test fail

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Thanh/i);
+  const linkElement = screen.getByText(/Test Fail/i);
   expect(linkElement).toBeInTheDocument();
 });
